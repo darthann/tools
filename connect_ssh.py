ba07f74bedc25a1f54173407c844b3427c399c02
darthann <younes.ijichi@gmail.com>
2019-06-20 11:58:24 -0400

    Update connect_ssh.py

--- connect_ssh.py
+++ connect_ssh.py
@@ -7,6 +7,10 @@ child = pexpect.spawn (cmd)
 child.expect("password")
 child.sendline("dg43zz6R0E")
 
-"Read content (Example with sudoku challenge)
+# Read content (Example with sudoku challenge)
 child.expect("Solve this sudoku")
 # child.before contains output of the server
+
+# To get last ouput of console
+child.expect(pexpect.EOF)
+print(child.before)
