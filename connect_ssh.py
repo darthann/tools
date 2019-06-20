7bdae5694fd0d8fd80de09170d6764d4d9c1fe73
darthann <younes.ijichi@gmail.com>
2019-06-20 11:40:32 -0400

    Create connect_ssh.py

---
+++ connect_ssh.py
@@ -0,0 +1,12 @@
+import pexpect
+
+cmd = "ssh sudoku@challenges.ringzer0team.com -p 10143"
+
+child = pexpect.spawn (cmd)
+# Server is going to ask for password
+child.expect("password")
+child.sendline("dg43zz6R0E")
+
+"Read content (Example with sudoku challenge)
+child.expect("Solve this sudoku")
+# child.before contains output of the server
